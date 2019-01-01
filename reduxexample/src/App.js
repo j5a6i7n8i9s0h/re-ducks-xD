0fccb98ed867160fd58f4a30e2268673436ce772
j5a6i7n8i9s0h <21962504@student.uwa.edu.au>
2019-01-01 22:21:06 +0800

    init

---
+++ reduxexample/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import "./App.css";
+import Posts from "./components/Posts";
+import PostForm from "./components/PostForm";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+
+const store = createStore(() => [], {}, applyMiddleware());
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div>
+          <PostForm />
+          <Posts />
+        </div>
+      </Provider>
+    );
+  }
+}
+
+export default App;
